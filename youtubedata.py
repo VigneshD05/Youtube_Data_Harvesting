d44e8c9df9b96b676204864d3e95519efa3987c2
VigneshD05 <159689942+VigneshD05@users.noreply.github.com>
2024-03-17 11:33:12 +0530

    Update youtubedata.py

--- youtubedata.py
+++ youtubedata.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 #API key connection
 def Api_connect():
-    Api_Id="AIzaSyA3T2nnb_7sbR1sBGjILP3sUmQ9nvql8s8"
+    Api_Id="<Api_Id>"
     api_service_name = "youtube"
     api_version = "v3"
     youtube = build(api_service_name,api_version,developerKey=Api_Id)
